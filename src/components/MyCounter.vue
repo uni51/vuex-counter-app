99051d77cdb7fe22a4ae0979bb83ac6b8f367dbf
uni51 <uni51@users.noreply.github.com>
2020-01-09 14:44:05 +0900

    Part 1 Vueアプリケーションの開発

---
+++ src/components/MyCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Count: {{ count }}
+    <button @click="onclick">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 1
+    }
+  },
+  methods: {
+    onclick () {
+      this.count++
+    }
+  }
+}
+</script>
